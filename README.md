f2a0dbf1b772cf032cdbd488dc67928812f9d78e
Jamie Gist <jamie.gist.7@gmail.com>
2022-08-27 22:02:19 -0700

    Add javascript.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # music-store
 Ecommerce website
+
+photo credit: Photo by Marlene Leppänen: https://www.pexels.com/photo/blue-disc-with-sleeve-1925099/
--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function openPage() {
-    tabcontent = document.getElementByClassName('tabcontent').style.display = 'none';
-    document.getElementById(shopping).style.display = "block";
+// Variables
+var tabs = document.getElementsByClassName('tabs');
+
+//Functions
+function openShop() {
+    var tabContent = document.getElementsByClassName('tab-content');
+for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].getElementsByClassName.display = "none";
+}
+for (i = 0; i < tabs.length; i++) {
+var tab = document.getElementsByClassName('tab');
+    tab[i].className = tab[i].className.replace(" active", "");
+}
 }
