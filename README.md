dd9ffeba2514d54c00b25b3c69e1f3c5186be27c
Jamie Gist <jamie.gist.7@gmail.com>
2022-08-31 00:32:02 -0700

    Styling and fleshed out shop page.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Ecommerce website
 
 photo credit: Photo by Marlene Leppänen: https://www.pexels.com/photo/blue-disc-with-sleeve-1925099/
+
+shop photo credits:
+Photo by Robin McPherson: https://www.pexels.com/photo/assorted-vinyl-record-lot-908965/
+
+Photo by Alina Vilchenko: https://www.pexels.com/photo/vinyl-disc-on-top-of-shelf-2117239/
+
+Photo by Lopsan: https://www.pexels.com/photo/makin-magic-album-sleeve-2191013/
--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 // Variables
-var tabs = document.getElementsByClassName('tabs');
+
 
 //Functions
-function openShop() {
-    var tabContent = document.getElementsByClassName('tab-content');
-for (i = 0; i < tabContent.length; i++) {
-    tabContent[i].getElementsByClassName.display = "none";
-}
-for (i = 0; i < tabs.length; i++) {
-var tab = document.getElementsByClassName('tab');
-    tab[i].className = tab[i].className.replace(" active", "");
-}
-}
