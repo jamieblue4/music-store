60dd58d795a1c379d59da5206d8ac8f6d330c89a
Jamie Gist <jamie.gist.7@gmail.com>
2022-10-20 22:49:13 -0700

    Add functions and edit some of the user input features.

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
 // Variables
-
+const name = document.querySelector('.name');
+const resetButton = document.querySelector('#reset');
+const logoutButton = document.querySelector('#logout');
+const form = document.querySelector('form');
+const getUserName = document.querySelector('#username');
+const submitButton = document.querySelector('#name-submit');
 
 //Functions
+
+form.addEventListener('submit', function(e) { 
+    e.preventDefault();
+});
+
+submitButton.addEventListener('click', function() {
+    localStorage.setItem('name', getUserName.value);
+
+    nameDisplayCheck();
+});
+
+submitButton.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter');
+}
+);
+
+logoutButton.addEventListener('click', function() {
+    localStorage.removeItem('name');
+    
+    nameDisplayCheck();
+});
+
+function nameDisplayCheck() {
+    if (localStorage.getItem('name')) {
+        let name = localStorage.getItem('name');
+        let personalGreeting = document.getElementById('.personal-greeting');
+        personalGreeting.appendChild('p');
+    } else {
+  
+    }
+}
