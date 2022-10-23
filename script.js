f41626e9b6374c8479ee48d6bc94a73e9abcdaf5
Jamie Gist <jamie.gist.7@gmail.com>
2022-10-23 16:06:41 -0700

    Fix bug where name wasn't storing into local storage.

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-
-const nameContainer = document.querySelector('.name');
 const resetButton = document.querySelector('#reset');
-const logoutButton = document.querySelector('#logout');
 const form = document.querySelector('form');
-const submitButton = document.querySelector('#name-submit');
+const submitButton = document.querySelector('#signup');
+const logoutButton = document.querySelector('#logout-button');
 
 //Functions
-submitButton.addEventListener('click', function() {
-    preventDefault();
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
 });
 
-submitButton.addEventListener('keypress', function(e) {
-    if(e.key === 'Enter');
-    preventDefault();
+submitButton.addEventListener('click', function() {
+    let name = document.querySelector('#username').value;
+    name = localStorage.setItem('name', name);
 });
 
 submitButton.addEventListener('keypress', function(e) {
     if (e.key === 'Enter');
-    let name = document.querySelector('#user-name');
-    localStorage.setItem('name', name.value);
-}
-);
-
-submitButton.addEventListener('click', function() {
-    let name = document.querySelector('#user-name');
-    localStorage.setItem('name', name.value);
+    let name = document.querySelector('#username').value;
+    name = localStorage.setItem('name', name);
 });
 
 logoutButton.addEventListener('click', function() {
-    let name = document.querySelector('#user-name');
-    localStorage.removeItem(name);
+    let name = localStorage.getItem('name');
+    localStorage.removeItem('name');
 });
