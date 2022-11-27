d99e02b1a503dbb2ae39a4e3e2b572223d94ae7b
Jamie Gist <jamie.gist.7@gmail.com>
2022-11-27 00:40:47 -0800

    Shopping cart section. Finishing up total section.

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ function nameDisplayCheck() {
 
 //Add to cart Functions and Shop Section
 var shop = document.querySelector('.shop-container');
+var cartItems = document.querySelector('.cart-container');
 
 const product = [
     {
@@ -71,7 +72,80 @@ const product = [
     }
 ];
 
+// empty cart array
+let cart = [];
 
+// Add to cart
 function addToCart(id) {
-  console.log(id);
+    if(cart.some((item) => item.id === id)){
+        changeNumberOfUnits("plus", id);
+    } else {
+        const item = product.find((product) => product.id === id);
+
+        cart.push({
+            ...item,
+            numberOfUnits: 1,
+        });
+      }
+      updateCart();
+    }
+ 
+// update cart
+function updateCart() {
+    renderCartItems();
+    //renderTotal();
+}
+
+// calculate and render subtotal
+function renderTotal() {
+    let totalPrice = 0, totalItems = 0;
+
+    cart.forEach((item) => {
+        totalPrice += item.price * item.numberOfUnits;
+        totalItems += item.oldNumberOfUnits;
+    });
+}
+
+// render cart items
+function renderCartItems() {
+    cartItems.innerHTML = ""; // clear cart before proceeding
+    cart.forEach((item) => {
+        cartItems.innerHTML += `
+            <div class="cart-content">
+            <h4 class="cart-name">
+            ${item.name}
+            </h4>
+            <h4 class="cart-price">
+            ${item.price}
+            </h4>
+            <h4 class="cart-quantity">
+            <div class="btn-minus" onclick="changeNumberOfUnits('minus', ${item.id})">-</div>
+            ${item.numberOfUnits}
+            <div class="btn-plus" onclick="changeNumberOfUnits('plus', ${item.id})">+</div>
+            </h4>
+        </div>
+            `;
+    });
+}
+
+// change number of units for an item
+function changeNumberOfUnits(action, id) {
+    cart = cart.map((item) => {
+
+        let oldNumberOfUnits = item.numberOfUnits;
+
+        if(item.id === id) {
+            if(action === "minus" && item.numberOfUnits > 1) {
+                oldNumberOfUnits--;
+            } else if (action === "plus") {
+                oldNumberOfUnits++;
+            }
+        }
+
+        return {
+        ...item,
+        numberOfUnits: oldNumberOfUnits,
+        };
+    });
+    updateCart();
 }
